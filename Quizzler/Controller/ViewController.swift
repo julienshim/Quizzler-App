261f9f6f28709a824cc4bc5027939706e33ee263
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-12-02 11:29:44 -0800

    ui update

--- Quizzler/Controller/ViewController.swift
+++ Quizzler/Controller/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     let allQuestions = QuestionBank()
     var pickedAnswer: Bool = false
     var questionNumber: Int = 0
+    var score: Int = 0
 
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -22,8 +23,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let firstQuestion = allQuestions.list[0]
-        questionLabel.text = firstQuestion.questionText
+        nextQuestion()
     }
 
     
@@ -35,13 +35,19 @@ class ViewController: UIViewController {
         }
         
 
-            checkAnswer()
+        checkAnswer()
             
-            questionNumber += 1
+        questionNumber += 1
         
         nextQuestion()
        
         
+    }
+    
+    func updateUI() {
+        scoreLabel.text = "Score: \(score)"
+        progressLabel.text = "\(questionNumber+1)/13"
+        progressBar.frame.size.width = (view.frame.size.width / 13) * CGFloat(questionNumber + 1)
     }
     
     func nextQuestion() {
@@ -49,6 +55,7 @@ class ViewController: UIViewController {
         if questionNumber <= 12 {
             
             questionLabel.text = allQuestions.list[questionNumber].questionText
+            updateUI()
             
         } else {
             
@@ -73,6 +80,7 @@ class ViewController: UIViewController {
         
         if correctAnswer == pickedAnswer {
             print("You got it!")
+            score += 1
         }  else {
             print("Wrong!")
         }
